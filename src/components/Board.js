62c905a2b2fbcc948a6bc3473940fa24c4b9f473
RinaHan <rinahan4146@gmail.com>
2024-01-30 22:22:40 -0800

    show who is a winner

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,10 +5,42 @@ import "./Board.css";
 const Board = () => {
   const [squares, setSquares] = useState([Array[9]].fill(null));
   const [xIsNext, setXIsNext] = useState(true);
-  const status = `Next player: ${xIsNext ? "X" : "0"}`;
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+      // console.log('squaresValue', squares)
+    }
+    return null;
+  };
+  const winner = calculateWinner(squares);
+
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = `Next player: ${xIsNext ? "X" : "0"}`;
+  }
 
   const handleClick = (i) => {
     const newSquares = squares.slice();
+    // console.log('squaresN', newSquares, i)
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
     newSquares[i] = xIsNext ? "X" : "0";
     setSquares(newSquares);
     setXIsNext((prev) => !prev);
